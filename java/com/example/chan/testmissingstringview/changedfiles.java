1174d56095e1e145dbe3bb4273dd48f56135575f
Cheng Han <chan@tripadvisor.com>
2014-10-09 15:25:39 -0400

    add a changed file to test

---
+++ java/com/example/chan/testmissingstringview/changedfiles.java
@@ -0,0 +1,9 @@
+package com.example.chan.testmissingstringview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
